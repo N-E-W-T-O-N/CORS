e2489334cc4c418a88e16434a0cc8c30b8fc9283
newton.mallick <newton.mallick@celebaltech.com>
2023-12-27 18:18:59 +0530

    First Commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using Core;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddCors(opt=>opt.AddPolicy("MyPolicy",a=>a.WithOrigin("doamin info")));  //<===
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseCors(opt=>opt.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()s); ///<<===
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
